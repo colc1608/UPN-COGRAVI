b0836a7fe6bea1de124e2fd5b05c6e7c32fb0aea
LAPTOP-SK9IT9J4\cesar <cesar.1608@outlook.com>
2021-08-22 01:31:32 -0500

    Commit inicial - version establet tiene la libreria configurada

---
+++ COGRAVI/Lienzo.cpp
@@ -0,0 +1,23 @@
+#include <GL/glut.h>
+/*
+void dibujar() {
+	glLoadIdentity();
+	gluOrtho2D(0, 10, 0, 10);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	//limpieza
+	glFlush();
+
+}
+
+int main(int argc, char* argv[]) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE);
+	glutInitWindowSize(800, 800);
+	glutInitWindowPosition(500, 500);
+	glutCreateWindow("CESAR LOPEZ");
+	glutDisplayFunc(dibujar);
+	glutMainLoop();
+
+	return 0;
+}*/
